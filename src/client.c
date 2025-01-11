de9bd50aeb1fead660d05afbbe83a944265e2e74
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 09:56:03 -0300

    feat: add client thread creation and handling function Added 'handle_client()' function to manage client-specific tasks in a separate thread. (not finished) Integrated thread creation in 'create_client()' function with error handling Included necessary headers for thread handling and error reporting

--- src/client.c
+++ src/client.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <errno.h>
 
 #include "../include/client.h"
+
+void* handle_client(void* arg) {
+    client_t* client = (client_t*)arg;
+
+    return NULL;
+}
 
 client_t* create_client(int clientfd, struct sockaddr_in client_addr) {
     client_t* client = malloc(sizeof(client_t));
@@ -11,7 +19,11 @@ client_t* create_client(int clientfd, struct sockaddr_in client_addr) {
     client->addr = client_addr;
     strcpy(client->username, "Guest"); 
 
-
+    if (pthread_create(&client->thread_id, NULL, handle_client, (void*)client)
+            != 0) {
+        printf("Failed to create client thread: %s\n", strerror(errno));
+        return NULL;
+    }
 
     return client;
 }
