b9defaf7025c03070fa6d8853ff6e47fac83a775
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 00:51:24 -0300

    feat: add function 'create_client()' fix client_t struct member name

--- include/client.h
+++ include/client.h
@@ -6,12 +6,12 @@
 
 typedef struct {
     int sockfd;
-    struct sockaddr_in;
+    struct sockaddr_in addr;
     char username[20];
     pthread_t thread_id;
 } client_t;
 
-
+client_t* create_client(int clientfd, struct sockaddr_in client_addr);
 
 #endif
 
