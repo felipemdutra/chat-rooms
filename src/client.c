484de5e8acb791339242c81797788b072aab2316
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 17:00:12 -0300

    reafactor client handling logic Add  function to handle client messages Finish  to print client messages to the terminal Update  to get client address using

--- src/client.c
+++ src/client.c
@@ -1,11 +1,24 @@
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 #include <errno.h>
+#include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include "../include/client.h"
+
+ssize_t read_message(client_t* client, char* buf, size_t buf_size) {
+    ssize_t bytes_read = recv(client->sockfd, buf, buf_size, 0);
+    if (bytes_read <= 0) {
+        printf("%s disconnected\n", client->username);
+        return -1;
+    }
+
+    return bytes_read;
+}
 
 /**
  * @param arg Pointer to client.
@@ -13,6 +26,19 @@
 void* handle_client(void* arg) {
     client_t* client = (client_t*)arg;
 
+    char buf[1024];
+    
+    while (1) {
+        ssize_t bytes_read = read_message(client, buf, sizeof(buf));
+        if (bytes_read < 0) {
+            break;
+        }
+        buf[bytes_read] = '\0';
+        printf("Received from %s: %s\n",client->username, buf);
+    }
+
+    close(client->sockfd);
+    pthread_exit(NULL);
     return NULL;
 }
 
@@ -22,12 +48,16 @@ void* handle_client(void* arg) {
  *
  * @return Pointer to the created client structure, or NULL on failure.
  */
-client_t* create_client(int clientfd, struct sockaddr_in client_addr) {
+client_t* create_client(int clientfd) {
     client_t* client = malloc(sizeof(client_t));
     if (!client) {
         printf("Failed to allocate memory for client\n");
         return NULL;
     }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen = sizeof(client_addr);
+    getpeername(clientfd, (struct sockaddr*)&client_addr, &client_addrlen);
 
     client->sockfd = clientfd;
     client->addr = client_addr;
