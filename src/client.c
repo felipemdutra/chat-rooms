9e02d5569eaf3b7d1f83b19899c8339b7518db59
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 12:14:20 -0300

    fix memory leak when thread creation fails add error handling when allocating memory for client_t document 'handle_client()' and 'create_client()' functions

--- src/client.c
+++ src/client.c
@@ -3,17 +3,31 @@
 #include <string.h>
 #include <pthread.h>
 #include <errno.h>
+#include <sys/types.h>
 
 #include "../include/client.h"
 
+/**
+ * @param arg Pointer to client.
+ */
 void* handle_client(void* arg) {
     client_t* client = (client_t*)arg;
 
     return NULL;
 }
 
+/**
+ * @param clientfd Client socket descriptor.
+ * @param client_addr Client address.
+ *
+ * @return Pointer to the created client structure, or NULL on failure.
+ */
 client_t* create_client(int clientfd, struct sockaddr_in client_addr) {
     client_t* client = malloc(sizeof(client_t));
+    if (!client) {
+        printf("Failed to allocate memory for client\n");
+        return NULL;
+    }
 
     client->sockfd = clientfd;
     client->addr = client_addr;
@@ -22,6 +36,7 @@ client_t* create_client(int clientfd, struct sockaddr_in client_addr) {
     if (pthread_create(&client->thread_id, NULL, handle_client, (void*)client)
             != 0) {
         printf("Failed to create client thread: %s\n", strerror(errno));
+        free(client);
         return NULL;
     }
 
