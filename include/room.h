f019d7813d08031d4b74b06748b78334f80d2ac3
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 23:17:40 -0300

    start working on chat rooms

--- include/room.h
+++ include/room.h
@@ -4,8 +4,13 @@
 #include "./client.h"
 
 typedef struct {
-
+    client_t** clients;
+    int curr_num_clients;
+    int max_clients;
+    char* room_name;
 } room_t;
+
+room_t* create_room(int max_clients, const char* room_name);
 
 #endif
 
