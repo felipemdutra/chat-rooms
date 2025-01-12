aac44a605ad3b8af9819e8ed0c3333cd7b778e79
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 23:42:56 -0300

    add  to room_t struct add creator parameter to  to handle room ownership and privilages

--- include/room.h
+++ include/room.h
@@ -4,13 +4,14 @@
 #include "./client.h"
 
 typedef struct {
+    client_t* leader;
     client_t** clients;
     int curr_num_clients;
     int max_clients;
     char* room_name;
 } room_t;
 
-room_t* create_room(int max_clients, const char* room_name);
+room_t* create_room(char *room_name, int max_clients, client_t* creator);
 
 #endif
 
