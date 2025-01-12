ea3edeef47652c1a398e09f3628efc5c2e112d9c
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-11 23:01:23 -0300

    no comment

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include "../include/server.h"
 
 int main() {
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
     if (start_server() < 0) {
         printf("Failed to start server");
         return 1;
