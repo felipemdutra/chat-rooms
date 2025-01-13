4407b8ea3e120ba1d99d254e1302a169e20c28f7
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-13 09:15:47 -0300

    feat: add room and messaging functionalities to client structure Added forward declaration for struct 'room_t' Updated 'client_t' struct to include 'struct room_t* curr_room' to track the current room of the client Added 'send_to_room' function prototype to send messages to a room Added 'send_message function prototype to send messages t oa specific client

--- include/client.h
+++ include/client.h
@@ -4,6 +4,8 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
+struct room_t;
+
 #define MAX_CLIENTS 100
 #define NAME_MAX_CHAR 20
 
@@ -12,6 +14,7 @@ typedef struct {
     struct sockaddr_in addr;
     char username[NAME_MAX_CHAR];
     pthread_t thread_id;
+    struct room_t* curr_room;
 } client_t;
 
 void* handle_client(void* arg);
@@ -19,6 +22,8 @@ client_t* create_client(int clientfd);
 ssize_t read_message(client_t* client, char* buf, size_t buf_size);
 int send_system_message(client_t* client, char* message);
 int send_global_message(client_t* sender, const char* message);
+int send_to_room(client_t* sender, const char* message);
+int send_message(client_t* sender, client_t* receiver, const char* message);
 
 int change_name(client_t* client, const char* new_name);
 
