137c05abb4d841b7a68080396ac7b4af9b0a4be2
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-12 21:35:52 -0300

    add more room functionality

--- include/room.h
+++ include/room.h
@@ -10,11 +10,12 @@ typedef struct {
     client_t** clients;
     int curr_num_clients;
     int max_clients;
-    char* room_name;
+    char room_name[NAME_MAX_CHAR];
 } room_t;
 
-room_t* create_room(char *room_name, int max_clients, client_t* creator);
-room_t* find_room(char* room_name);
+room_t* create_room(const char *room_name, int max_clients, client_t* creator);
+int delete_room(const char* room_name, client_t* owner);
+room_t* find_room(const char* room_name);
 int join_room(client_t* client, char* room_name);
 
 #endif
