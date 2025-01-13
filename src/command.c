c9f9381d4b5c20ced0d416da148534d4ba79c2bf
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-13 09:31:40 -0300

    feat: implement room listing functionality Add support for the 'list' command to display all available rooms to the client Use 'get_rooms()' to retrieve the list of rooms and dynamically allocate memory for the array of rooms Handle cases where no rooms are available or retrieval fails, sending appropriate system messages to the client Ensure dynamically allocated room arrays are properly freed after use to prevent memory leaks Improve eror handling for 'create_room()' by returning early if the room creation fails

--- src/command.c
+++ src/command.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <string.h>
+
+extern int room_count;
 
 #include "../include/client.h"
 #include "../include/command.h"
@@ -22,11 +25,35 @@ void handle_commands(client_t* client, char* buf) {
     }
 
     if (strcmp(command, "create") == 0) {
-        room_t* room = create_room(args, 10, client);
+        if (create_room(args, 10, client) == NULL) {
+            return;
+        }
     }
 
     if (strcmp(command, "join") == 0) {
         join_room(client, args);
+    }
+
+    if (strcmp(command, "list") == 0) {
+       room_t** rooms = get_rooms();
+        if (!rooms) {
+            send_system_message(client, "Failed to retrieve rooms list");
+            return;
+        }
+
+        if (room_count == 0) {
+            send_system_message(client, "No rooms available");
+            return;
+        } 
+        char room_list[1024] = "Available rooms:\n";
+        
+        for (int i = 0; i < room_count; i++) {
+            strncat(room_list, rooms[i]->room_name, sizeof(room_list) - strlen(room_list) - 1);
+            strncat(room_list, "\n", sizeof(room_list) - strlen(room_list) - 1);
+        }
+
+        send_system_message(client, room_list);
+        free(rooms);
     }
 }
 
