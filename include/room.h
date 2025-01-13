ebe4eee65d4509bd1237a8a5e6f5e6e727fd6c39
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-13 09:18:34 -0300

    feat: update room structure and functionalities Defined 'ROOM_NAME_MAX_CHAR' for room names Changed 'room_name' array size in 'room_t' sruct to use 'ROOM_NAME_MAX_CHAR' Added forward declaration of struct 'room_t' and included 'client.h' after defining constants Added new function prototype 'get_rooms' to retrieve the list of rooms

--- include/room.h
+++ include/room.h
@@ -1,22 +1,25 @@
 #ifndef ROOM_H
 #define ROOM_H
 
-#include "./client.h"
-
 #define MAX_ROOMS 15
+#define ROOM_NAME_MAX_CHAR 20
 
-typedef struct {
+#include "client.h"
+
+typedef struct room_t {
     client_t* leader;
     client_t** clients;
     int curr_num_clients;
     int max_clients;
-    char room_name[NAME_MAX_CHAR];
+    char room_name[ROOM_NAME_MAX_CHAR];
 } room_t;
+
 
 room_t* create_room(const char *room_name, int max_clients, client_t* creator);
 int delete_room(const char* room_name, client_t* owner);
 room_t* find_room(const char* room_name);
 int join_room(client_t* client, char* room_name);
+room_t** get_rooms();
 
 #endif
 
